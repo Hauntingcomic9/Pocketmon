e79dac089ac15d60d066011fd5d7b15a7191ad5f
ALEX-DAWSON <ktad2001@gmail.com>
2019-12-19 14:20:54 -0500

    First Commit

---
+++ src/pocketmon/Pocketmon.java
@@ -0,0 +1,22 @@
+/*
+ * Alex Dawson and Nick Robichaud
+ * 19-Dec-2019
+ * --------------------------------------
+ * Pocketmon/Pocketmon.java
+ */
+package pocketmon;
+
+/**
+ *
+ * @author ktad2
+ */
+public class Pocketmon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
